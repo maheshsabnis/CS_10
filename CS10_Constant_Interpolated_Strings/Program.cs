ae54f867a691cde618ddee53572b0f6f8e49bbd0
maheshsabnis <sabnis_m@hotmail.com>
2022-01-28 12:27:54 +0530

    All

---
+++ CS10_Constant_Interpolated_Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CS10_ConstantStringInterpolation;
+
+public static class ConstaniStringInterpolation
+{
+    private const string BaseUrl = "/data/api";
+
+    public static class Urls
+    {
+        public const string GetAll = $"{BaseUrl}/Employees";
+        // C# 10 Constant String Interpolation
+        public const string GetById = $"{BaseUrl}/{{eno:guid}}";
+        // Traditional Concatination
+        public const string GetByName = BaseUrl + "/{name:string}";
+        // Earlier
+        public static readonly string GetByLastName  = $"{BaseUrl}/{{eno:guid}}";
+    }
+}
+
---
+++ CS10_FileScopedNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿//  C# 10 Code 
+
+namespace MyNamespace;
+
+    internal class MyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    internal class MyClass1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
